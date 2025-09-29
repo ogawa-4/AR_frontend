1c7f2f3e3f0bd740e7f7255108f4051a6b95f605
ogawa-4 <yonrog163@gmail.com>
2025-09-29 14:11:14 +0900

    Viewページピンなおし

--- src/pages/View.jsx
+++ src/pages/View.jsx
@@ -3,6 +3,19 @@ import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
 import 'leaflet/dist/leaflet.css';
 import { useNavigate } from 'react-router-dom';
 import './View.css';
+import L from 'leaflet';
+import markerIcon2x from 'leaflet/dist/images/marker-icon-2x.png';
+import markerIcon from 'leaflet/dist/images/marker-icon.png';
+import markerShadow from 'leaflet/dist/images/marker-shadow.png';
+
+// デフォルトアイコンを再設定
+delete L.Icon.Default.prototype._getIconUrl;
+
+L.Icon.Default.mergeOptions({
+  iconRetinaUrl: markerIcon2x,
+  iconUrl: markerIcon,
+  shadowUrl: markerShadow,
+});
 
 export default function View() {
     const navigate = useNavigate();
